79585816f4557892b47ecaac7a9c30b6e3e02f81
Olayiwola <waleadejnr@gmail.com>
2023-05-22 08:33:53 +0100

    improved UI

--- public/browser.js
+++ public/browser.js
@@ -1,24 +1,22 @@
 function itemTemplate(item) {
-  return `
-  <li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
+  return `<li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
   <span class="item-text">${item.text}</span>
   <div>
-    <button data-id="${item._id}" class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
-    <button data-id="${item._id}" class="delete-me btn btn-danger btn-sm">Delete</button>
+  <button data-id="${item._id}" class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
+  <button data-id="${item._id}" class="delete-me btn btn-danger btn-sm">Delete</button>
   </div>
-</li>`;
+  </li>`;
 }
-//initial page load render
+
+// Initial Page Load Render
 let ourHTML = items
   .map(function (item) {
     return itemTemplate(item);
   })
-  .join();
-
+  .join("");
 document.getElementById("item-list").insertAdjacentHTML("beforeend", ourHTML);
 
-//create feature
-
+// Create Feature
 let createField = document.getElementById("create-field");
 
 document.getElementById("create-form").addEventListener("submit", function (e) {
@@ -43,30 +41,26 @@ document.getElementById("create-form").addEventListener("submit", function (e) {
 });
 
 document.addEventListener("click", function (e) {
-  //delete
+  // Delete Feature
   if (e.target.classList.contains("delete-me")) {
-    if (confirm("Are you sure you want to delete permanently?")) {
+    if (confirm("Do you really want to delete this item permanently?")) {
       axios
-        .post("/delete-item", {
-          id: e.target.getAttribute("data-id"),
-        })
+        .post("/delete-item", { id: e.target.getAttribute("data-id") })
         .then(function () {
           e.target.parentElement.parentElement.remove();
         })
         .catch(function () {
-          console.log("Try again later");
+          console.log("Please try again later.");
         });
     }
   }
 
-  //update
+  // Update Feature
   if (e.target.classList.contains("edit-me")) {
-    // alert("ffd");
     let userInput = prompt(
-      "Enter your new text",
+      "Enter your desired new text",
       e.target.parentElement.parentElement.querySelector(".item-text").innerHTML
     );
-    // console.log(userInput);
     if (userInput) {
       axios
         .post("/update-item", {
@@ -79,7 +73,7 @@ document.addEventListener("click", function (e) {
           ).innerHTML = userInput;
         })
         .catch(function () {
-          console.log("Try again later");
+          console.log("Please try again later.");
         });
     }
   }
--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 let express = require("express");
-let app = express();
 let { MongoClient, ObjectId } = require("mongodb");
-let sanitzeHTML = require("sanitize-html");
+let sanitizeHTML = require("sanitize-html");
 
+let app = express();
 let db;
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
 
 app.use(express.static("public"));
 
@@ -13,7 +18,7 @@ async function go() {
   );
   await client.connect();
   db = client.db();
-  app.listen(3000);
+  app.listen(port);
 }
 
 go();
@@ -22,12 +27,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 function passwordProtected(req, res, next) {
-  res.set("WWW-Authenticate", 'basic realm= "Simple Todo App"');
+  res.set("WWW-Authenticate", 'Basic realm="Simple Todo App"');
   console.log(req.headers.authorization);
   if (req.headers.authorization == "Basic ZnVsbDpzdGFjaw==") {
     next();
   } else {
-    res.status(401).send("authentication required");
+    res.status(401).send("Authentication required");
   }
 }
 
@@ -35,60 +40,53 @@ app.use(passwordProtected);
 
 app.get("/", async function (req, res) {
   const items = await db.collection("items").find().toArray();
-  //   console.log(items);
-  res.send(`
-  <!DOCTYPE html>
-<html>
-<head>
+  res.send(`<!DOCTYPE html>
+  <html>
+  <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Simple To-Do App</title>
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
-</head>
-<body>
+  </head>
+  <body>
   <div class="container">
-    <h1 class="display-4 text-center py-1">To-Do App</h1>
-    
-    <div class="jumbotron p-3 shadow-sm">
-      <form id='create-form' action = '/create-item' method = 'POST'>
-        <div class="d-flex align-items-center">
-          <input id='create-field' name='item' autofocus autocomplete="off" class="form-control mr-3" type="text" style="flex: 1;">
-          <button class="btn btn-primary">Add New Item</button>
-        </div>
-      </form>
-    </div>
-    
-    <ul id='item-list' class="list-group pb-5">
+  <h1 class="display-4 text-center py-1">To-Do App!</h1>
   
-    </ul>
-    
-    </div>
-    <script>
-          let items = ${JSON.stringify(items)}
-    </script>
-  <script src="https://unpkg.com/axios@1.1.2/dist/axios.min.js"></script>
-  <script src='/browser.js'></script>
+  <div class="jumbotron p-3 shadow-sm">
+  <form id="create-form" action="/create-item" method="POST">
+  <div class="d-flex align-items-center">
+  <input id="create-field" name="item" autofocus autocomplete="off" class="form-control mr-3" type="text" style="flex: 1;">
+  <button class="btn btn-primary">Add New Item</button>
+  </div>
+  </form>
+  </div>
+  
+  <ul id="item-list" class="list-group pb-5">
+  </ul>
+  
+  </div>
+  
+  <script>
+  let items = ${JSON.stringify(items)}
+  </script>
 
-</body>
-</html>
-  `);
+  <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
+  <script src="/browser.js"></script>
+  </body>
+  </html>`);
 });
 
 app.post("/create-item", async function (req, res) {
-  let safeText = sanitzeHTML(req.body.text, {
+  let safeText = sanitizeHTML(req.body.text, {
     allowedTags: [],
     allowedAttributes: {},
   });
   const info = await db.collection("items").insertOne({ text: safeText });
   res.json({ _id: info.insertedId, text: safeText });
-  // res.redirect("/");
-  // res.send("Success!");
 });
 
 app.post("/update-item", async function (req, res) {
-  //   console.log(req.body.text);
-  //   res.send("Success");
-  let safeText = sanitzeHTML(req.body.text, {
+  let safeText = sanitizeHTML(req.body.text, {
     allowedTags: [],
     allowedAttributes: {},
   });
@@ -102,8 +100,6 @@ app.post("/update-item", async function (req, res) {
 });
 
 app.post("/delete-item", async function (req, res) {
-  //   console.log(req.body.text);
-  //   res.send("Success");
   await db.collection("items").deleteOne({ _id: new ObjectId(req.body.id) });
   res.send("Success");
 });
